0617383d2e0627a70c72ed58f144051527325820
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-12-18 20:37:07 +0330

    Add files via upload

---
+++ debianTingsToDo.sh
@@ -0,0 +1,49 @@
+# things to do after installing lovely debian
+
+# update packages
+sudo apt update
+
+# remove extra packages
+sudo apt -y purge \
+xiterm+thai \
+mlterm mlterm-common mlterm-tools \
+xterm \
+thunderbird \
+gnome-maps \
+goldendict \
+hdate-applet \
+gnome-todo \
+rhythmbox \
+mozc-* \
+transmission-* \
+shotwell \
+gnome-sound-recorder
+
+
+
+# autoremove
+sudo apt -y autoremove
+
+# upgrade packages
+sudo apt -y upgrade
+
+# install needed packages
+sudo apt -y install \
+bash-completion \
+git \
+neofetch \
+network-manager-openvpn-gnome \
+vlc
+
+
+echo "bind 'set completion-ignore-case on'" >> ~/.bashrc
+
+
+
+
+
+
+
+
+
+
