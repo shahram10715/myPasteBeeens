383b37dd91bb268b406991b23870c1789150eb5a
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-12-27 15:43:15 +0330

    Create deb2.sh

---
+++ deb2.sh
@@ -0,0 +1,28 @@
+# things to do after installing lovely debian
+
+# update packages
+sudo apt update
+
+
+# upgrade packages
+sudo apt -y upgrade
+
+
+# autoremove
+sudo apt -y autoremove
+
+
+# install needed packages
+sudo apt -y install \
+bash-completion \
+gnome-multi-writer \
+git \
+neofetch \
+network-manager-openvpn-gnome \
+vlc
+
+
+echo "bind 'set completion-ignore-case on'" >> ~/.bashrc
+
+
+
