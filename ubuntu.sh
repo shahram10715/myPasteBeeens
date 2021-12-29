c1dd40ec78f88245ee0c65587ab4c69117dfd659
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-12-29 15:16:48 +0330

    Add files via upload

---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+sudo apt update
+sudo apt -y upgrade
+
+###########################
+
+sudo apt -y purge \
+update-manager \
+software-properties-* 
+
+
+
+###########################
+sudo apt -y install \
+git \
+neofetch \
+gnome-multi-writer \
+gnome-shell-extensions \
+gnome-tweak-tool
+
+
+
