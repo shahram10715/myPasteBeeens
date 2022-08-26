a0064568473195437316705020b4e502b1b1289c
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2022-08-26 17:55:33 +0430

    Add files via upload

---
+++ mintAfterInstall.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt purge -y libreoffice*
+sudo apt purge -y hexchat thunderbird mintreport mintupdate hypnotix timeshift warpinator redshift
+sudo apt install -y virtualbox
+
+
+
+
+
+
+
+
